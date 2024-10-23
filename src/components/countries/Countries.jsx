8318e6b78fa07b1a4acf26f49c8c2049eea8f74a
hasibprangon <hasibulhossainp09@gmail.com>
2024-10-23 20:05:42 +0600

    showing more data and style added

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,24 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import Country from '../country/Country';
-
+import './countries.css'
 const Countries = () => {
     const [countries, setCountries] = useState([]);
 
     useEffect(() => {
         const url = `https://restcountries.com/v3.1/all`;
         fetch(url)
-        .then(res => res.json())
-        .then(data => setCountries(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setCountries(data))
+    }, [])
     return (
         <div>
-           <h3>Countries: {countries.length} </h3> 
-           {
-            countries.map(country => <Country
-            key={country.cca3}
-            country={country}
-            ></Country>)
-           }
+            <h3>Countries: {countries.length} </h3>
+            <div className='countryContainer'>
+                {
+                    countries.map(country => <Country
+                        key={country.cca3}
+                        country={country}
+                    ></Country>)
+                }
+            </div>
         </div>
     );
 };
--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import './country.css'
 const Country = ({country}) => {
     console.log(country);
-    const {name, flags} = country;
+    const {name, flags, area, population, cca2} = country;
     return (
         <div className='country'>
             <img src={flags.png} alt="" />
             <h3>Name: {name.common}</h3>
+            <p>Population: {population}</p>
+            <p>Area: {area}</p>
+            <p><small>Country Code: {cca2}</small></p>
         </div>
     );
 };
